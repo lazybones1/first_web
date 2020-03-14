410e1ab9ed5de77b913e0c64b8d2c8ee5b0111a0
Jinsan <53360337+lazybones1@users.noreply.github.com>
2020-03-14 12:34:39 +0900

    Add files via upload

--- web2-nodejs/main.js
+++ web2-nodejs/main.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 
-function templateHTML(title, list, body){
+function templateHTML(title, list, body, control){
   return `
   <!doctype html>
   <html>
@@ -14,7 +14,7 @@ function templateHTML(title, list, body){
   <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
-    <a href="/create">create</a>
+    ${control}
     ${body}
   </body>
   </html>
@@ -42,18 +42,23 @@ var app = http.createServer(function(request,response){
           var title = 'Welcome';
           var description = 'Hello, Node.js';
           var list = templateList(filelist);
-          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`, `<a href="/create">create</a>`);
           response.writeHead(200);
           response.end(template);
         });
       } else {
         fs.readdir('./data', function(error, filelist){
-          var title = 'Welcome';
-          var description = 'Hello, Node.js';
-          var list = templateList(filelist);
           fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
             var title = queryData.id;
-            var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+            var list = templateList(filelist);
+            var template = templateHTML(title, list,
+              `<h2>${title}</h2>${description}`,
+              `<a href="/create">create</a>
+               <a href="/update?id=${title}">update</a>
+               <form action="delete_process" method="post" >
+                <input type = "hidden" name="id" value="${title}">
+                <input type ="submit" value="delete">
+               </form>`);
             response.writeHead(200);
             response.end(template);
           });
@@ -64,12 +69,12 @@ var app = http.createServer(function(request,response){
         var title = 'WEB - create';
         var list = templateList(filelist);
         var template = templateHTML(title, list, `
-          <form action="http://localhost:3000/create_process" method="post">
+          <form action="/create_process" method="post">
           <p><input type="text" name="title" placeholder = "title"></p>
           <p><textarea name="description" placeholder = "description"></textarea></p>
           <p><input type="submit" value="Submit"></p>
-        </form>`);
-        response.writeHead(200);
+        </form>`,'');
+        response.writeHead(200); // 200: 작업 성공
         response.end(template);
       });
     }else if(pathname === '/create_process'){
@@ -81,10 +86,48 @@ var app = http.createServer(function(request,response){
         var post = qs.parse(body);
         var title = post.title;
         var description = post.description;
-        console.log(post);
+        //파일 만들고 쓰기
+        fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+          response.writeHead(302, {Location: `/?id=${title}`});//302 : 페이지를 redirection시키기
+          response.end();
+        })
       });
-      response.writeHead(200);
-      response.end('Success');
+    }else if(pathname === '/update'){
+      fs.readdir('./data', function(error, filelist){
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+          var title = queryData.id;
+          var list = templateList(filelist);
+          var template = templateHTML(title, list,
+            `
+            <form action="/update_process" method="post">
+              <input type = "hidden" name="id" value="${title}">
+              <p><input type="text" name="title" placeholder = "title" value="${title}"></p>
+              <p><textarea name="description" placeholder = "description">${description}</textarea></p>
+              <p><input type="submit" value="Submit"></p>
+            </form>
+            `,
+            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
+          response.writeHead(200);
+          response.end(template);
+        })
+      });
+    }else if(pathname === '/update_process'){
+      var body = '';
+      request.on('data', function(data){
+        body = body + data;
+      });
+      request.on('end', function(end){
+        var post = qs.parse(body);
+        var id = post.id;
+        var title = post.title;
+        var description = post.description;
+        fs.rename(`data/${id}`, `data/${title}`, function(err){
+          fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+            response.writeHead(302, {Location: `/?id=${title}`});//302 : 페이지를 redirection시키기
+            response.end();
+          })
+        })
+      });
     }else {
       response.writeHead(404);
       response.end('Not found');
